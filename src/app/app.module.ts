8a78bd6150d978232dc416cb98985f94ff1a24a1
PedroVale1 <162756423+PedroVale1@users.noreply.github.com>
2025-02-06 18:35:08 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { PetsPageComponent } from './pets-page/pets-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    ProductsPageComponent,
+    PetsPageComponent
   ],
   imports: [
     BrowserModule,
