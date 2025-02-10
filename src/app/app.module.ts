aadbaea6b3ca165b897bbd9b6cd43a2ca13d9df7
PedroVale1 <162756423+PedroVale1@users.noreply.github.com>
2025-02-09 22:20:34 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
@@ -11,6 +11,7 @@ import { ProductsPageComponent } from './pages/store/products-page/products-page
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { FramePageComponent } from './pages/master/frame.page';
+import { ProductCardComponent } from './components/store/product-card/product-card.component';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import { FramePageComponent } from './pages/master/frame.page';
     PetsPageComponent,
     CartPageComponent,
     FramePageComponent,
+    ProductCardComponent,
   ],
   imports: [
     BrowserModule,
