28a04a15985dcc5c9bb3e2c53d0c5c6ca1fa8f3a
PedroVale1 <162756423+PedroVale1@users.noreply.github.com>
2025-03-01 21:43:37 -0300

    Add files via upload

--- src/app/components/store/product-card/product-card.component.ts
+++ src/app/components/store/product-card/product-card.component.ts
@@ -1,16 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Product } from 'src/app/models/product.model';
+import { Component, Input, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { Product } from "src/app/models/product.model";
+import { CartUtil } from "src/app/utils/cart.util";
 
 @Component({
-  selector: 'app-product-card',
-  templateUrl: './product-card.component.html'
+  selector: "app-product-card",
+  templateUrl: "./product-card.component.html",
 })
 export class ProductCardComponent implements OnInit {
-@Input() product: Product;
+  @Input() product: Product;
 
-  constructor() { }
+  constructor() {}
+  private toastr: ToastrService;
+  ngOnInit() {}
 
-  ngOnInit() {
+  addToCart() {
+    CartUtil.add(
+      this.product._id,
+      this.product.title,
+      1,
+      this.product.price,
+      this.product.image
+    );
+    this.toastr.success(
+      `${this.product.title} adicionado ao carrinho`,
+      "Produto Adicionado"
+    );
   }
-
 }
